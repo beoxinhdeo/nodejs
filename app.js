1f1cfc369b6325943792dd68148eb1733b0dd7fc
lyngocmyphuong <lyngocmyphuong@gmail.com>
2019-09-23 13:25:37 +0700

    changed

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+server.listen(3000);
+
+app.get("/", function(req,res)
+{
+    //res.send("<font color=red >beoxinhdep</font>")
+    res.sendFile(__dirname + "/index.html")
+})
+app.get("/gioithieu.apx", function(req,res)
+{
+    //res.send("<font color=red >beoxinhdep</font>")
+    //res.sendFile(__dirname + "/index.html")
+    res.send("im beoxinhdep");
+})
+app.get("/tinhtong/:so1/:so2", function(req,res)
+{
+    var n = req.params.so1;
+    n = parseInt(n);
+    
+    var m = req.params.so2;
+    m = parseInt(m);
+    
+    var tong = n+m;
+    res.send("<font color=red >"+tong+"</font>")
+    //res.sendFile(__dirname + "/index.html")
+})
